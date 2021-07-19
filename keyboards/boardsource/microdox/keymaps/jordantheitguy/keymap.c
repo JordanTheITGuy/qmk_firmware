bc0410d06a94540f630e3e2ef7baafa9acde5fc5
Jordan Benzing <jordan.benzing@gmail.com>
2021-07-19 08:36:59 -0400

    Update keymap.c

--- keyboards/boardsource/microdox/keymaps/jordantheitguy/keymap.c
+++ keyboards/boardsource/microdox/keymaps/jordantheitguy/keymap.c
@@ -37,22 +37,20 @@ enum custom_keycodes {          // Make sure have the awesome keycode ready
 enum {
     TD_LBRAC_LPAREN,
     TD_RBRAC_RPAREN,
-    TD_DASH_V,
-    TD_UNDER_M,
 };
 
 
 
 
-#define KC_CTSC RCTL_T(KC_SCLN)
+#define KC_CTSC LT(_NUMPAD,KC_SCLN)
 #define RAISE_SPC LT(_RAISE, KC_SPC)
 #define RASE_BACK LT(_RAISE, KC_BSPC)
-#define NUM_S LT(_NUMPAD, KC_S)
-#define NUM_L LT(_NUMPAD,KC_L)
-#define ALT_D MT(MOD_LALT, KC_D)
-#define ALT_K MT(MOD_RALT,KC_K)
+#define ALT_S MT(MOD_LALT, KC_S)
+#define ALT_L MT(MOD_RALT,KC_L)
+#define CTRL_D MT(MOD_LCTL, KC_D)
+#define CTRL_K MT(MOD_RCTL,KC_K)
 #define LOWER_ENT LT(_LOWER, KC_ENT)
-#define CTRL_A MT(MOD_LCTL,KC_A)
+#define NUM_A LT(_NUMPAD,KC_A)
 #define SHFT_F MT(MOD_LSFT, KC_F)
 #define SHFT_J MT(MOD_RSFT, KC_J)
 #define TAB_LOWER LT(_LOWER, KC_TAB)
@@ -69,46 +67,44 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Escape, twice for Caps Lock
     [TD_LBRAC_LPAREN] = ACTION_TAP_DANCE_DOUBLE(KC_LCBR, KC_LBRC),
     [TD_RBRAC_RPAREN] = ACTION_TAP_DANCE_DOUBLE(KC_RCBR, KC_RBRC),
-    [TD_UNDER_M] = ACTION_TAP_DANCE_DOUBLE(KC_M, KC_UNDS),
-    [TD_DASH_V] = ACTION_TAP_DANCE_DOUBLE(KC_V, KC_MINS),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QWERTY] = LAYOUT_split_3x5_3( 
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  
-        CTRL_A,  NUM_S,    ALT_D,    SHFT_F,    KC_G,         KC_H,    SHFT_J,    ALT_K, NUM_L,   KC_CTSC, 
-        KC_Z,  KC_X,    KC_C,    TD(TD_DASH_V), KC_B,               KC_N,    TD(TD_UNDER_M),    KC_COMM, KC_DOT, KC_SLASH, 
-                  KC_LGUI, TAB_LOWER, RAISE_SPC,                RASE_BACK, LOWER_ENT, ALT_TAB
+  [_QWERTY] = LAYOUT_split_3x5_3(
+        KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
+        NUM_A,  ALT_S,    CTRL_D,    SHFT_F,    KC_G,       KC_H,    SHFT_J,   CTRL_K, ALT_L,   KC_CTSC,
+        KC_Z,    KC_X,    KC_C,    KC_V, KC_B,              KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLASH,
+                KC_FFXIV, TAB_LOWER, RAISE_SPC,              RASE_BACK, LOWER_ENT, ALT_TAB
                 ),
-  [_RAISE] = LAYOUT_split_3x5_3( 
-        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,        KC_HOMER, KC_HOME, KC_UP, KC_END, KC_ENDER, 
-        XXXXXXX, XXXXXXX, KC_COPY, KC_PASTE, KC_DEL,        KC_DEL, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUOT, 
-        KC_LSFT, KC_GRV, XXXXXXX, XXXXXXX, XXXXXXX,         KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, 
-                            _______, _______, _______,          _______,  _______, _______
+  [_RAISE] = LAYOUT_split_3x5_3(
+        KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,        KC_HOMER, KC_HOME, KC_UP, KC_END, KC_ENDER,
+        XXXXXXX, XXXXXXX, KC_COPY, KC_PASTE, KC_DEL,        KC_DEL, KC_LEFT, KC_DOWN, KC_RGHT, KC_QUOT,
+        KC_LSFT, KC_GRV, XXXXXXX, XXXXXXX, XXXXXXX,         KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
+                            _______, _______, _______,          KC_GAMER,  KC_FFXIV, _______
                          ),
-  [_LOWER] = LAYOUT_split_3x5_3( 
+  [_LOWER] = LAYOUT_split_3x5_3(
         KC_F2, KC_F3, KC_F5, KC_F8, KC_F12,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-        _______,   _______, KC_LPRN, TD(TD_LBRAC_LPAREN), _______,           _______,TD(TD_RBRAC_RPAREN) , KC_RPRN, _______, KC_DQT, 
-        XXXXXXX, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX,             KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, 
+        _______,   _______, KC_LPRN, TD(TD_LBRAC_LPAREN), _______,           _______,TD(TD_RBRAC_RPAREN) , KC_RPRN, _______, KC_DQT,
+        XXXXXXX, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX,             KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                             _______,  _______,  _______,             _______,  _______, _______
                          ),
     [_NUMPAD] = LAYOUT_split_3x5_3(
           KC_ESC, XXXXXXX, KC_MS_UP, KC_PSCR, XXXXXXX,             XXXXXXX, KC_7, KC_8, KC_9, XXXXXXX,
           XXXXXXX, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, XXXXXXX,            XXXXXXX, KC_4, KC_5, KC_6, XXXXXXX,
           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, KC_1, KC_2, KC_3, XXXXXXX,
-                            KC_GAMER, KC_FFXIV, KC_MS_BTN1,               KC_0, _______, _______
+                            _______,_______ , KC_MS_BTN1,               KC_0, _______, _______
     ),
     [_GAMING] = LAYOUT_split_3x5_3(
-          XXXXXXX, XXXXXXX, KC_W, KC_R, XXXXXXX,             XXXXXXX, KC_7, KC_8, KC_9, XXXXXXX,
+          KC_LALT, KC_Q, KC_W, KC_E, XXXXXXX,             XXXXXXX, KC_7, KC_8, KC_9, XXXXXXX,
           KC_LSFT, KC_A, KC_S, KC_D, KC_G,                   XXXXXXX, KC_4, KC_5, KC_6, XXXXXXX,
           XXXXXXX, XXXXXXX, XXXXXXX, KC_V, XXXXXXX,            XXXXXXX, KC_1, KC_2, KC_3, XXXXXXX,
-                            _______, KC_TAB, KC_SPC,               KC_0, _______, _______
+                            _______, KC_TAB, KC_SPC,               KC_0, KC_GAMER, _______
     ),
     [_FFXIV] = LAYOUT_split_3x5_3(
-          XXXXXXX, KC_Q, KC_W, KC_E, XXXXXXX,             XXXXXXX, KC_7, KC_8, KC_9, XXXXXXX,
-          KC_LSFT, KC_A, KC_S, KC_D, KC_G,                   XXXXXXX, KC_4, KC_5, KC_6, XXXXXXX,
-          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, KC_1, KC_2, KC_3, XXXXXXX,
-                            _______, KC_TAB, KC_SPC,               KC_0, _______, _______
+          KC_ESC, KC_Q, KC_W, KC_E, XXXXXXX,             XXXXXXX, KC_7, KC_8, KC_9, XXXXXXX,
+          KC_LCTRL, KC_A, KC_S, KC_D, KC_G,                   XXXXXXX, KC_4, KC_5, KC_6, XXXXXXX,
+          KC_LALT, XXXXXXX, KC_C, XXXXXXX, XXXXXXX,            XXXXXXX, KC_1, KC_2, KC_3, XXXXXXX,
+                            _______, KC_TAB, KC_SPC,               KC_0, KC_FFXIV, _______
     )
 };
 
@@ -154,7 +150,7 @@ static void render_logo(void) {
     0
   };
   oled_write_P(qmk_logo, false);
-}                                                                                 
+}
 
 static void render_status(void) {
    switch (get_highest_layer(layer_state)) {
@@ -179,12 +175,12 @@ static void render_status(void) {
       oled_write_P(PSTR("Layer: Numpad\n"), false);
       break;
     case _GAMING:
-      oled_write_P(PSTR("B R L N G O"), false);
+      oled_write_P(PSTR("B R L N G O\n"), false);
       oled_write_P(PSTR("        ^\n"), false);
       oled_write_P(PSTR("Layer: GAMING\n"), false);
       break;
     case _FFXIV:
-      oled_write_P(PSTR("FFXIV"), false);
+      oled_write_P(PSTR("FFXIV\n"), false);
       oled_write_P(PSTR("Welcome to Eorzea\n"), false);
       oled_write_P(PSTR("Layer: FFXIV\n"), false);
       break;
